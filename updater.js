b9ede60c1a5f061d56d6526734feafac811878c2
Clartik <clartikdev@gmail.com>
2025-12-31 22:17:59 -0800

    add: version text to window title

--- updater.js
+++ updater.js
@@ -3,8 +3,8 @@ const { autoUpdater } = require('electron-updater');
 function setupUpdater() {
     const log = require('electron-log');
     log.transports.file.level = "debug";
-
     autoUpdater.logger = log;
+
     autoUpdater.checkForUpdatesAndNotify();
 }
 
